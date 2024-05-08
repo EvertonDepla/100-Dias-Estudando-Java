e8755e3fe59b24382b24d537a8ef685db8b873ca
Everton Deplá <evertondutra257@gmail.com>
2024-05-08 20:15:02 -0300

    meu primeiro commit

---
+++ src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Everton";
+        String segundoNome = "Deplá";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
+    }
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
