a7d3fe8151f10ec03f2221fcf74f4f5ca105419d
Everton Deplá <evertondutra257@gmail.com>
2024-05-09 22:22:45 -0300

    dia 2

---
+++ src/edu/everton/dia2/TiposVariaveis.java
@@ -0,0 +1,24 @@
+package edu.everton.dia2;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+        
+        String meuNome = "EVERTON DEPLÁ";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
