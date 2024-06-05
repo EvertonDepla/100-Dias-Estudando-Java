29c17ce8be5232684294bcb7276561137ba96e91
Everton Deplá <evertondutra257@gmail.com>
2024-06-04 22:00:14 -0300

    Dia 28

---
+++ src/edu/everton/dia28/Computador.java
@@ -0,0 +1,30 @@
+package edu.everton.dia28;
+
+import edu.everton.dia27.ServicoMensagemInstantanea;
+import edu.everton.dia27.Telegram;
+import edu.everton.dia27.MSNMessenger;
+import edu.everton.dia27.Facebook;
+
+public class Computador {
+    public static void main(String[] args) {
+		
+		ServicoMensagemInstantanea smi = null;
+		
+		/*
+		    NÃO SE SABE QUAL APP 
+		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+		String appEscolhido="msn"; 
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new Facebook();
+		else if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+		
+			
+		smi.enviarMensagem();
+		smi.receberMensagem();
+	}
+}
