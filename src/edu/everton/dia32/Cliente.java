422268df14d540724c7d8c44b25316b5c414341d
Everton Deplá <evertondutra257@gmail.com>
2024-06-08 22:08:44 -0300

    Dia 32

---
+++ src/edu/everton/dia32/Cliente.java
@@ -0,0 +1,14 @@
+package edu.everton.dia32;
+
+public class Cliente {
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome + nome;
+    }
+}
---
+++ src/edu/everton/dia32/ContaPoupança.java
@@ -0,0 +1,31 @@
+package edu.everton.dia32;
+
+public class ContaPoupança {
+    
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar() {
+
+    }
+    public void depositar() {
+
+    }
+    public void transferir() {
+        
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
+
