c6d2231d791265c5d3aac9064136a9ed3c1c4d64
Everton Deplá <evertondutra257@gmail.com>
2024-06-09 20:51:38 -0300

    Dia 33

---
+++ src/edu/everton/dia33/Carro.java
@@ -0,0 +1,41 @@
+package edu.everton.dia33;
+
+    public class Carro implements Veiculo {
+        private int velocidade;
+        private int marcha;
+    
+        public Carro() {
+            this.velocidade = 0;
+            this.marcha = 1;
+        }
+    
+        @Override
+        public void acelerar() {
+            velocidade += 10;
+            System.out.println("O carro acelerou. Velocidade atual: " + velocidade + " km/h");
+        }
+    
+        @Override
+        public void frear() {
+            velocidade -= 10;
+            if (velocidade < 0) {
+                velocidade = 0;
+            }
+            System.out.println("O carro freiou. Velocidade atual: " + velocidade + " km/h");
+        }
+    
+        @Override
+        public void trocarMarcha(int marcha) {
+            this.marcha = marcha;
+            System.out.println("O carro trocou para a marcha " + this.marcha);
+        }
+    
+        public static void main(String[] args) {
+            Carro meuCarro = new Carro();
+            meuCarro.acelerar();
+            meuCarro.acelerar();
+            meuCarro.frear();
+            meuCarro.trocarMarcha(3);
+        }
+    }
+
---
+++ src/edu/everton/dia33/Veiculo.java
@@ -0,0 +1,7 @@
+package edu.everton.dia33;
+
+public interface Veiculo {
+    void acelerar();
+    void frear();
+    void trocarMarcha(int marcha);
+}
