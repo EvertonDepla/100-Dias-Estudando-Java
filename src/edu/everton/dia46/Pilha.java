1440be8f9b665530fba534dc85af23b90841df56
Everton Deplá <evertondutra257@gmail.com>
2024-06-22 23:33:32 -0300

    DIA 46

---
+++ src/edu/everton/dia46/Pilha.java
@@ -0,0 +1,37 @@
+package edu.everton.dia46;
+
+public class Pilha {
+
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public void push(No novoNo) {
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+
+    public No pop() {
+        if(!this.isEmpty()) {
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+            return noPoped;
+        }
+        return  null;
+    }
+
+    public No top() {
+      return   refNoEntradaPilha;
+    }
+
+    public boolean isEmpty() {
+//        if(refNoEntradaPilha == null) {
+//            return true;
+//        }
+//        return false;
+        return  refNoEntradaPilha == null ? true : false;
+    }
+}
