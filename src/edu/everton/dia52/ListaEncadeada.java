a274b3b4bcbc17e50ab735b99d51f634b3388609
Everton Deplá <evertondutra257@gmail.com>
2024-06-28 23:37:53 -0300

    Dia 52

---
+++ src/edu/everton/dia52/ListaEncadeada.java
@@ -0,0 +1,33 @@
+package edu.everton.dia52;
+
+public class ListaEncadeada<T> {
+
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada(){
+        this.referenciaEntrada = null;
+    }
+
+    public  int size(){
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+        while (true){
+            if(referenciaAux != null){
+                tamanhoLista++;
+                if(referenciaAux.getProximoNo() != null){
+                    referenciaAux = referenciaAux.getProximoNo();
+                }else{
+                    break;
+                }
+            }else {
+                break;
+            }
+        }
+
+        return tamanhoLista;
+    }
+
+    public boolean isEmpty(){
+        return referenciaEntrada == null ? true : false;
+    }
+}
