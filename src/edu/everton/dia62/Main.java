79a08fb017fddc4633d05b287bb564a0c7c231be
Everton Deplá <evertondutra257@gmail.com>
2024-07-08 21:18:02 -0300

    Dia 62

---
+++ src/edu/everton/dia62/Main.java
@@ -0,0 +1,24 @@
+package edu.everton.dia62;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Audi"));
+        listaCarros.add(new Carro("Ferrari"));
+
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println(new Carro("Ford").hashCode());
+
+        Carro carro1 = new Carro("Ford");
+        Carro carro2 = new Carro("Audi");
+
+        System.out.println(carro1.equals(carro2));
+    }
+}
