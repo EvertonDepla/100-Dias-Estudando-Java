4807fe66fc055b8fb5303b6c888423e086d33bf5
Everton Deplá <evertondutra257@gmail.com>
2024-07-09 22:48:34 -0300

    Dia 63

---
+++ src/edu/everton/dia63/Carro.java
@@ -0,0 +1,39 @@
+package edu.everton.dia63;
+
+import java.util.Objects;
+
+public class Carro {
+
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Carro carro = (Carro) object;
+        return Objects.equals(marca, carro.marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(marca); }
+
+        @Override
+        public String toString() {
+            return "Carro{" +
+                    "marca=" + marca +
+                    '}';
+        }
+}
