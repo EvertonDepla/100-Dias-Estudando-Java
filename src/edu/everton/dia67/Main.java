639f891e72b3eb1d50a9e97a3ba66d367cca7e6a
Everton Deplá <evertondutra257@gmail.com>
2024-07-13 22:27:17 -0300

    Dia 67

---
+++ src/edu/everton/dia67/Main.java
@@ -0,0 +1,41 @@
+package edu.everton.dia67;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<String, String> aluno = new HashMap<>();
+
+        aluno.put("Nome", "Everton");
+        aluno.put("Idade", "23");
+        aluno.put("Media", "8.5");
+        aluno.put("Turma", "3a");
+
+        System.out.println(aluno);
+
+        System.out.println(aluno.keySet());
+        System.out.println(aluno.values());
+
+        List<Map<String, String>> listaAlunos = new ArrayList<>();
+
+        listaAlunos.add(aluno);
+
+        Map<String, String> aluno2 = new HashMap<>();
+
+        aluno2.put("Nome", "Maria");
+        aluno2.put("Idade", "20");
+        aluno2.put("Media", "6.1");
+        aluno2.put("Turma", "3a");
+
+        listaAlunos.add(aluno2);
+
+        System.out.println(listaAlunos);
+
+        System.out.println(aluno.containsKey("Nome"));
+    }
+}
