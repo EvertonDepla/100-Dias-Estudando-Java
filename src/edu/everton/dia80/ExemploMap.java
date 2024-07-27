df7028cbbe1b3d436294946aa5b4c77495ef46c3
Everton Deplá <evertondutra257@gmail.com>
2024-07-26 22:08:17 -0300

    Dia 80

---
+++ src/edu/everton/dia80/ExemploMap.java
@@ -0,0 +1,52 @@
+package edu.everton.dia80;
+
+import java.util.*;
+
+//        Map carrosPopulares2020 = new HashMap(); //antes do java 5
+//        Map<String, Double> carrosPopulares = new HashMap<>(); //Generics(jdk 5) - Diamont Operator(jdk 7)
+//        HashMap<String, Double> carrosPopulares = new HashMap<>();
+//        Map<String, Double> carrosPopulares2020 = Map.of("gol", 14.4, "uno", 15.6, "mobi", 16.1, "hb20", 14.5, "kwid", 15.6)
+
+public class ExemploMap {
+    public static void main(String[] args) {
+
+        System.out.println("Crie um dicionário que relacione os modelos e seus respectivos consumos");
+
+        Map<String, Double> carrosPopulares = new HashMap<>(){{
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+        }};
+        System.out.println(carrosPopulares);
+
+        System.out.println("Substitua o consumo do gol por 15,2 km/l: ");
+        carrosPopulares.put("gol", 15.2);
+        System.out.println(carrosPopulares);
+
+        System.out.println("Confira se o modelo tucson está no dicionário " + carrosPopulares.containsKey("uno"));
+        System.out.println("Exiba o consumo do Uno " + carrosPopulares.get("uno"));
+
+        System.out.println("Exiba os modelos: ");
+        Set<String> modelos = carrosPopulares.keySet();
+        System.out.println(modelos);
+
+        System.out.println("Exiba os consumos dos carros: ");
+        Collection<Double> consumos = carrosPopulares.values();
+        System.out.println(consumos);
+
+        System.out.println("Exiba o modelo mais econômico e seu consumo: ");
+
+        Double consumoMaisEficiente = Collections.max(carrosPopulares.values());
+        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();
+        String modeloMaisEficiente = "";
+
+        for (Map.Entry<String, Double> entry: entries) {
+            if (entry.getValue().equals(consumoMaisEficiente)) {
+                modeloMaisEficiente = entry.getKey();
+                System.out.println("Modelo mais efiociente: " + modeloMaisEficiente + " - " + consumoMaisEficiente);
+            }
+        }
+    }
+}
